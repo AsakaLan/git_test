af2e1322abe62d423d481db9910de80691218f2b
codehang <103125315+AsakaLan@users.noreply.github.com>
2023-07-07 23:38:15 +0800

    Update math.go
    add hello world

--- math.go
+++ math.go
@@ -4,6 +4,11 @@ package test
 func Add(x,y int )(res int ){
 	return x + y 
 }
+fmt.println("hello world")
+fmt.println("hello world")
+fmt.println("hello world")
+fmt.println("hello world")
+fmt.println("hello world")
 func Sub(x,y int )(res int ){
 	return x - y 
 }
